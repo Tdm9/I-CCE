5119f91c327753a26a26bbc53bcf7044ab9ccddc
Tdm9 <48017174+Tdm9@users.noreply.github.com>
2021-03-01 12:05:09 +0000

    changing deployment domain again

--- src/vue-plugins/router.js
+++ src/vue-plugins/router.js
@@ -5,17 +5,17 @@ import Popular from "@/components/pages/Popular";
 import Search from "@/components/pages/Search";
 import Vue from "vue";
 import Meat from "@/components/pages/RecipeTypeViewer";
-
+import {publicPath} from '../../vue.config'
 const router = new VueRouter({
     mode: 'history',
     hash: false,
     routes: [
-        { path: '', component: Home },
-        { path: '/favourites', component: Favourites },
-        { path: '/popular', component: Popular },
-        { path: '/search', component: Search },
-        { path: '/typeSearch/:type', component: Meat },
-        { path: '*', redirect: '/' },
+        { path: `${publicPath}`, component: Home },
+        { path: `${publicPath}/favourites`, component: Favourites },
+        { path: `${publicPath}/popular`, component: Popular },
+        { path: `${publicPath}/search`, component: Search },
+        { path: `${publicPath}/typeSearch/:type`, component: Meat },
+        { path: '*', redirect: `${publicPath}` },
     ]});
 Vue.use(VueRouter);
 
